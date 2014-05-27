a4cbf24f4c8a195d3be4ba1fce3b90b8d3caeaf2
Luiz Gonzaga dos Santos Filho <lfilho@gmail.com>
2014-05-27 14:12:40 -0300

    Atualizando Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,32 @@
 module.exports = function(grunt) {
+    grunt.initConfig({
 
-  // Project configuration.
-  grunt.initConfig({
+        //make 2 packages and choose zepto if browser if new enough. This choice is made in default.html.eco
+        uglify: {
+            dist: {
+                "src": [
+                    "src/documents/scripts/modernizr.js",
+                    "node_modules/foundation/js/vendor/zepto.js",
+                    "node_modules/foundation/js/foundation/foundation.js",
+                    "node_modules/foundation/js/foundation/foundation.topbar.js"
+                ],
+                "dest": "out/scripts/zepto-pack.min.js"
+            },
+            "jquery-pack": {
+                "src": [
+                    "src/documents/scripts/modernizr.js",
+                    "node_modules/foundation/js/vendor/jquery.js",
+                    "node_modules/foundation/js/foundation/foundation.js",
+                    "node_modules/foundation/js/foundation/foundation.topbar.js"
+                ],
+                "dest": "out/scripts/jquery-pack.min.js"
+            }
+        }
+    });
 
-    //make 2 packages and choose zepto if browser if new enough. This choice is made in default.html.eco
-    uglify: {
-      dist: {
-        "src": [
-          "src/documents/scripts/modernizr.js",
-          "node_modules/foundation/js/vendor/zepto.js",
-          "node_modules/foundation/js/foundation/foundation.js",
-          "node_modules/foundation/js/foundation/foundation.topbar.js"
-        ],
-        "dest": "out/scripts/zepto-pack.min.js"
-      },
-      "jquery-pack": {
-        "src": [
-          "src/documents/scripts/modernizr.js",
-          "node_modules/foundation/js/vendor/jquery.js",
-          "node_modules/foundation/js/foundation/foundation.js",
-          "node_modules/foundation/js/foundation/foundation.topbar.js"
-        ],
-        "dest": "out/scripts/jquery-pack.min.js"
-      }
-    }
-  });
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
-
+    // Default task(s).
+    grunt.registerTask('default', ['uglify']);
 };
